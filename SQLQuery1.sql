40c08d2d324679c7893f8a927860d0d23f7bde3e
vishal <vishalpalode9899@gmail.com>
2022-03-26 15:32:06 +0530

    [VishalPalode] Added Emp Payroll table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,12 @@
 ---Welcome to SQL Project---
-Create database Payroll_Service
+Create database Payroll_Service;
 use Payroll_Service
+
+---UC2--Create table for database---
+create table employee_payroll
+(
+	Id int identity (1,1) primary Key,
+	Name varchar(200),
+	Salary float,
+	StartDate date
+);
